d38d45d2b07ea443cd079e4da17a79aadd104a5b
btcyberpubli <btcyberpubli@gmail.com>
2026-01-08 15:59:52 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const BUSINESS_CONFIG = {
   businessName: 'Northway Digital Services Ltd',
   businessAddress: 'Unit 6, Arundel Court, Netherthorpe Road, Sheffield, S1 4QS, United Kingdom',
-  businessPhone: '+44 114 275 7911',
+  businessPhone: '+44 7353924439',
   businessEmail: 'contact@northwaydigital.co.uk',
   websiteDomain: 'northwaydigital.co.uk',
   businessType: 'Digital Marketing Agency'
 };
+
